6bcd98bfef68d0a5dffd9cb6d39d2706e39d14e5
Jeff <jcubeta@algernon.solutions>
2018-10-16 16:48:14 -0400

    introduced source logic to manage return calls

--- toll_booth/alg_tasks/find_tasks/find_object.py
+++ toll_booth/alg_tasks/find_tasks/find_object.py
@@ -8,8 +8,13 @@ from toll_booth.alg_tasks.task_obj import remote_task
 def find_object(*args, **kwargs):
     logging.info(f'started a find object call with args {args} and kwargs {kwargs}')
     task_args = kwargs['task_args']
-    internal_id = task_args.get('internal_id', None)
-    object_type = task_args.get('object_type', None)
-    ogm = OgmReader()
-    if internal_id and object_type:
-        return ogm.find_object(internal_id, object_type == 'Edge')
+    context = task_args['context']
+    source = task_args['source']
+    if context:
+        internal_id = context.get('internal_id', None)
+        object_type = context.get('object_type', None)
+        ogm = OgmReader()
+        if internal_id and object_type:
+            return ogm.find_object(internal_id, object_type == 'Edge')
+    if source:
+        logging.info(str(source))
