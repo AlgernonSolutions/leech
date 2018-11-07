e4e6e7f879a417a95ff75cbabbe0b74548c560e1
Jeff <jcubeta@algernon.solutions>
2018-11-07 10:26:01 -0500

    increased sqs batch size after actually learning what the hell it does

--- tests/units/test_load.py
+++ tests/units/test_load.py
@@ -3,14 +3,16 @@ import pytest
 from toll_booth.alg_tasks.toll_booth import work
 
 test_event = {'task_name': 'load', 'task_args': {'keys': {'sid_value': {'S': '3178'}, 'identifier_stem': {'S': '#vertex#ExternalId#{"id_source": "MBI", "id_type": "Employees", "id_name": "emp_id"}#'}}}}
+other_event = {'task_name': 'load', 'task_args': {'keys': {'sid_value': {'S': '3134'}, 'identifier_stem': {'S': '#vertex#ExternalId#{"id_source": "MBI", "id_type": "Employees", "id_name": "emp_id"}#'}}}}
 
 
 @pytest.mark.test_load
-@pytest.mark.usefixtures('silence_x_ray', 'blank_table')
+@pytest.mark.usefixtures('silence_x_ray')
 class TestLoad:
     @pytest.mark.parametrize(
         'event', [
-            test_event
+            test_event,
+            other_event
         ]
     )
     def test_load(self, event, mock_context, mock_neptune):
