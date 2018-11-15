0e14173789494745379b021e62b01e29b8c12780
Jeff <jcubeta@algernon.solutions>
2018-11-15 12:27:46 -0500

    fixed malformation in template

--- toll_booth/alg_tasks/metl_tasks/monitor_object.py
+++ toll_booth/alg_tasks/metl_tasks/monitor_object.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 from aws_xray_sdk.core import xray_recorder
 
 from toll_booth.alg_obj.aws.sapper.leech_driver import LeechDriver
+from toll_booth.alg_obj.forge.fungi import Spore
 from toll_booth.alg_obj.forge.lizards import MonitorLizard
 from toll_booth.alg_obj.forge.mouse import MonitorMouse
 from toll_booth.alg_obj.graph.ogm.regulators import IdentifierStem
@@ -12,10 +13,10 @@ from toll_booth.alg_tasks.task_obj import remote_task
 
 @remote_task
 @xray_recorder.capture('monitor_object')
-def bulk_monitor(*args, **kwargs):
+def monitor_object(*args, **kwargs):
     logging.info('starting a monitor object task with args/kwargs: %s/%s' % (args, kwargs))
     task_args = kwargs['task_args']
-    lizard = MonitorLizard(**task_args)
-    results = lizard.bulk_monitor()
+    shroom = Spore(**task_args)
+    results = shroom.monitor()
     logging.info('finished a monitor object task with results: %s' % results)
     return results
