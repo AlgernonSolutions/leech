032cbefe74da125d64afac39ecd835bbc93ad0ea
Jeff <jcubeta@algernon.solutions>
2018-12-06 10:08:17 -0500

    fixed level of parameters passed to remote function

--- tests/units/test_clerk_swarm.py
+++ tests/units/test_clerk_swarm.py
@@ -1,7 +1,10 @@
+import pytest
+
 from toll_booth.alg_obj.aws.matryoshkas.clerks import ClerkSwarm
 
 
 class TestClerkSwarm:
+    @pytest.mark.test_clerks
     def test_clerk_swarm(self):
         entries = [{'identifier_stem': 'test_item', 'sid_value': str(x), 'is_test': True} for x in range(0, 25)]
         clerks = ClerkSwarm('VdGraphObjects', pending_entries=entries)
--- toll_booth/alg_obj/aws/matryoshkas/clerks.py
+++ toll_booth/alg_obj/aws/matryoshkas/clerks.py
@@ -9,6 +9,23 @@ class ClerkSwarm:
         self._table_name = table_name
         self._pending_entries = pending_entries
         self._max_retries = max_retries
+        self._receipt = {}
+
+    @property
+    def batched_entries(self):
+        batches = []
+        entries = []
+        counter = 0
+        for entry in self._pending_entries:
+            if len(entries) >= 25:
+                batches.append({'entries': entries})
+                entries = []
+            counter += 1
+            self._receipt[str(counter)] = entry
+            entries.append({'Item': entry})
+        if entries:
+            batches.append({'entries': entries})
+        return batches
 
     def __enter__(self):
         return self
@@ -24,14 +41,16 @@ class ClerkSwarm:
 
     def send(self):
         from toll_booth.alg_obj.aws.matryoshkas.matryoshka import Matryoshka, MatryoshkaCluster
-        receipt = {}
+        self._pending_entries = []
+        if not self.batched_entries:
+            return True
         send_task_name = 'batch_dynamo_write'
         lambda_arn = os.environ['WORK_FUNCTION']
         task_params = {'table_name': self._table_name}
         logging.info('preparing to send a batch of write commands through the clerk swarm')
         m_cluster = MatryoshkaCluster.calculate_for_concurrency(
             100, send_task_name, lambda_arn,
-            task_args=self._pending_entries, task_constants=task_params, max_m_concurrency=25)
+            task_args=self.batched_entries, task_constants=task_params, max_m_concurrency=25)
         m = Matryoshka.for_root(m_cluster)
         logging.info('completed the clerk send')
         agg_results = m.aggregate_results
