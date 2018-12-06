0f1e3572b29cab4e02104eb8f007e13912b1eac2
Jeff <jcubeta@algernon.solutions>
2018-12-06 09:53:35 -0500

    fixed up the remote tasks to include the new task type

--- toll_booth/alg_tasks/remote_tasks/__init__.py
+++ toll_booth/alg_tasks/remote_tasks/__init__.py
@@ -1,2 +1,2 @@
 from toll_booth.alg_tasks.remote_tasks import unpack_matryoshka, send_message_batch, \
-    update_schema, get_schema, explode
+    update_schema, get_schema, explode, batch_dynamo_write
