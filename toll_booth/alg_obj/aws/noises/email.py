a6e03f16973839a17b0b71369538d53401b8f87c
Michael Schappacher <wisdominlyrics@gmail.com>
2018-12-11 13:57:15 -0500

    Began work on the email component of the POSTS.
    This will allow users to receive custom reports in either CSV or Email format.

---
+++ toll_booth/alg_obj/aws/noises/email.py
@@ -0,0 +1,240 @@
+import json
+
+import boto3
+
+"""
+This module allows for the separation of data by organizations, then subdivided by admin/clinical/qi reports
+for individuals.
+
+Data is then formatted in an appropriate way and distributed to subscribers
+
+This module accepts JSON data objects. Defined within the EmailDataExtractor object.
+
+This drives the identification of subscribers, as well as the data that will be submitted to them.
+
+This module is predicated on the definition of canned reports, as well as creation of custom reports.
+
+"""
+
+
+class EmailDriver:
+    """
+    Utilizes the boto3 library in order to send an email.
+    """
+
+    def __init__(self, organization_dict: object):
+        self.client = boto3.client('ses',
+                                   aws_access_key_id="blank",
+                                   aws_secret_access_key="blank",
+                                   )
+        self.organization_dict = organization_dict
+
+    def send_email(self):
+            for email_subscriber in self.organization_dict.subscribers:
+                email = email_subscriber.email
+                data = EmailForm(email_subscriber)
+
+
+class EmailForm:
+    """
+    Formats the data and injects it into an email format. This is then submitted to the EmailDriver to be sent on to the
+    recipient
+    """
+
+    def __init__(self, email_subscriber: object):
+        self.email_subscriber = email_subscriber
+        self.email_data = {
+            "csv": [],
+            "email": []
+        }
+
+    def create_view(self):
+        pass
+
+    def create_csv(self):
+        pass
+
+    def create_email(self):
+        pass
+
+
+class OrganizationDict:
+    """
+    Collection of EmailSubscribers organized into a dictionary based on company.
+    example:
+
+    {
+    "mbi": [EmailSubscriber],
+    "psi": [EmailSubscriber]
+    }
+
+    """
+    def __init__(self):
+        self.organization_dict = {}
+
+    def add_subscriber(self, email_subscriber: object):
+        if email_subscriber.organization in self.organization_dict:
+            self.organization_dict[email_subscriber.organization].append(email_subscriber)
+        else:
+            self.organization_dict[email_subscriber.organization] = []
+            self.organization_dict[email_subscriber.organization].append(email_subscriber)
+
+
+class EmailSubscriber:
+    """
+    An individual, who is setup to receive an email. Formatted to his or her liking.
+    json representation:
+
+    *view is what privilege level the individual has to view data in the system.
+        admin = all teams
+        team = only the assigned team
+
+    {
+    "first_name": "Joe",
+    "last_name": "Schmo",
+    "email": "jschmo@gmail.com",
+    "team": "CSP 1",
+    "organization": "MBI",
+    "view": "admin",
+    "reports": {
+        "admin": {"payroll": "csv", "bonus": "email"},
+        "clinical": {"tx": "csv", "da": "email"}
+    }
+    """
+    def __init__(self, **kwargs):
+        self.first_name = kwargs.get("first_name")
+        self.last_name = kwargs.get("last_name")
+        self.email = kwargs.get("email")
+        self.team = kwargs.get("team")
+        self.organization = kwargs.get("organization")
+        self.view = kwargs.get("view")
+        self.reports = kwargs.get("reports")
+
+
+class EmailDataExtractor:
+    """
+    Converts data into various metrics, and reports. Then returns the reports back to the
+        data = {
+        "organization": {
+            "mbi": {
+                "clients": [
+                    {
+                        "first_name": "Joe",
+                        "last_name": "Schmo",
+                        "team": "CSP 1",
+                        "id": "1111",
+                        "csw_first_name": "John",
+                        "csw_last_name": "Test",
+                        "tx_plan_start": "1/1/1111 1:11:11",
+                        "tx_plan_end": "1/7/1111 1:11:11",
+                        "da_plan_start": "1/1/1111 1:11:11",
+                        "da_plan_end": "1/1/1112 1:11:11",
+                        "services_past_week": 11,
+                        "services_past_month": 39,
+                        "auths": {
+                            "diagnostic": "1/1",
+                            "community_supt": "245/300",
+                            "psychiatry": "22/30"
+                        }
+                    },
+                    {
+                        "first_name": "Mr",
+                        "last_name": "Smith",
+                        "team": "CSP 1",
+                        "id": "1112",
+                        "csw_first_name": "John",
+                        "csw_last_name": "Test",
+                        "tx_plan_start": "1/1/1111 1:11:11",
+                        "tx_plan_end": "1/7/1111 1:11:11",
+                        "da_plan_start": "1/1/1111 1:11:11",
+                        "da_plan_end": "1/1/1112 1:11:11",
+                        "services_past_week": 11,
+                        "services_past_month": 39,
+                        "auths": {
+                            "diagnostic": "1/1",
+                            "community_supt": "245/300",
+                            "psychiatry": "22/30"
+                            }
+                    }
+                ],
+                "services": [
+                    {
+                        "service_id": "102546",
+
+                    }
+                ],
+                "users": [
+                         {
+                            "first_name": "Joe",
+                            "last_name": "Schmo",
+                            "email": "jschmo@gmail.com",
+                            "team": "CSP 1",
+                            "view": "admin",
+                            "reports": {
+                                "admin": {"payroll": "csv", "bonus": "email"},
+                                "clinical": {"tx": "csv", "da": "email"}
+                                }
+                        },
+                        {
+                            "first_name": "John",
+                            "last_name": "Test",
+                            "email": "jschmo@gmail.com",
+                            "team": "CSP 1",
+                            "view": "admin",
+                            "reports": {
+                                "admin": {"payroll": "csv", "bonus": "email"},
+                                "clinical": {"tx": "csv", "da": "email"}
+                        }
+                        }
+                ]
+            },
+            "psi": {
+
+            },
+            "icfs": {
+
+            }
+        }
+    }
+    """
+    def __init__(self, data: json):
+        self.data = data
+
+    def create_user_list(self):
+        """
+         Parses through the JSON object in order to generate a list of users, then creates an OrganizationDict from the
+         users contained in the data.
+        """
+        org_dict = OrganizationDict()
+        for organization in self.data["organization"]:
+            if "users" in self.data["organization"][organization]:
+                for user in self.data["organization"][organization]["users"]:
+                    new_user = EmailSubscriber(
+                        first_name=user["first_name"],
+                        last_name=user["last_name"],
+                        email=user["email"],
+                        team=user["team"],
+                        organization=organization,
+                        view=user["view"],
+                        reports=user["reports"],
+                    )
+                    self.create_report_views(new_user.reports)
+                    org_dict.add_subscriber(new_user)
+            else:
+                continue
+        return org_dict
+
+    def create_report_views(self, report_data):
+        pass
+
+
+class UserReports:
+    """
+    defines and creates the canned and custom reports necessary fo each individual.
+    """
+    def __init__(self, subscriber: object):
+        self.subscriber = subscriber
+
+    def tx_report(self):
+        self.subscriber
+
