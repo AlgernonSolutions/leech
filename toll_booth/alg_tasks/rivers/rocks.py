0c15687b933f3abd112955dc052a3e1d349a934a
Jeff <jcubeta@algernon.solutions>
2019-01-14 13:07:18 -0500

    fixed task list finding for tasks

--- toll_booth/alg_tasks/rivers/rocks.py
+++ toll_booth/alg_tasks/rivers/rocks.py
@@ -10,7 +10,7 @@ from toll_booth.alg_obj.aws.snakes.snakes import StoredData
 
 def _conscript_ruffian(work_history, start_subtask_decision, leech_config):
     workflow_id = start_subtask_decision.workflow_id
-    open_ruffians = work_history.timer_history.open_ruffian_tasks
+    open_ruffians = work_history.marker_history.open_ruffian_tasks
     if workflow_id in open_ruffians:
         return
     workflow_config = leech_config.get_workflow_config(start_subtask_decision.type_name)
