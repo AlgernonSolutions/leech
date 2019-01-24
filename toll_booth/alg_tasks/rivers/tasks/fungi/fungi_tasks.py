d57e050e9532545e5f6dc6d62703c98a97d7abb3
Jeff <jcubeta@algernon.solutions>
2019-01-24 14:05:52 -0500

    minor mutation fix

--- toll_booth/alg_tasks/rivers/tasks/fungi/fungi_tasks.py
+++ toll_booth/alg_tasks/rivers/tasks/fungi/fungi_tasks.py
@@ -188,9 +188,10 @@ def generate_remote_id_change_data(**kwargs):
     changed_target = _build_changed_targets(id_source, extracted_data, change_action)
     if changed_target:
         returned_data['changed_target'] = changed_target
-    change_target = enriched_data['change_detail'].get(change_date_utc, None)
-    if change_target is not None:
-        returned_data['change_target'] = change_target
+    change_details = enriched_data.get('change_detail', {})
+    change_detail_target = change_details.get(change_date_utc, None)
+    if change_detail_target is not None:
+        returned_data['change_target'] = change_detail_target
     return {'change_data': returned_data}
 
 
