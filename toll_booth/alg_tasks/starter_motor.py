d85f2c2985307a01151b47fd3c695bf3bc334f6f
Jeff <jcubeta@algernon.solutions>
2019-03-06 14:28:27 -0500

    introduced ruffian rousing to starter motor

--- toll_booth/alg_tasks/starter_motor.py
+++ toll_booth/alg_tasks/starter_motor.py
@@ -3,13 +3,14 @@ from datetime import datetime
 
 import boto3
 
-from toll_booth.alg_obj.aws.gentlemen.tasks import Versions
+from toll_booth.alg_obj.aws.gentlemen.tasks import Versions, LeechConfig
+from toll_booth.alg_obj.aws.ruffians.ruffian import RuffianRoost
 from toll_booth.alg_tasks.lambda_logging import lambda_logged
 
 
 @lambda_logged
 def start_flow(event, context):
-    logging.info(f'recieved a call to fire a start_flow task, event: {event}')
+    logging.info(f'received a call to fire a start_flow task, event: {event}')
     provided_flow_id = event['flow_id']
     flow_id = _generate_flow_id(provided_flow_id)
     domain_name = event['domain_name']
@@ -17,6 +18,8 @@ def start_flow(event, context):
     input_string = event.get('input_string')
     client = boto3.client('swf')
     versions = Versions.retrieve(domain_name)
+    config = LeechConfig.retrieve()
+    _rouse_ruffians(domain_name, flow_id, flow_name, config)
     start_args = {
         'domain': domain_name,
         'workflowId': flow_id,
@@ -32,6 +35,13 @@ def start_flow(event, context):
         start_args['input'] = input_string
     start_results = client.start_workflow_execution(**start_args)
     return start_results
+
+
+def _rouse_ruffians(domain_name, flow_id, flow_name, leech_config):
+    workflow_config = leech_config.get_workflow_config(flow_name)
+    labor_task_lists = workflow_config.get('labor_task_lists', [])
+    execution_arns = RuffianRoost.conscript_ruffians(flow_id, labor_task_lists, domain_name, leech_config)
+    return execution_arns
 
 
 def _generate_flow_id(provided_flow_id):
