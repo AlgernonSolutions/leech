2c015f25bc98ef62d1c8c6929d5ef46e289067bd
Jeff <jcubeta@algernon.solutions>
2019-03-22 12:50:44 -0400

    added oversee function to template

--- toll_booth/alg_tasks/cleaner.py
+++ toll_booth/alg_tasks/cleaner.py
@@ -12,12 +12,16 @@ def cleaner(event, context):
     if 'overseer_token' in event:
         if 'error' in event:
             error = event['error']
-            client.respond_activity_task_failed(
-                taskToken=event['overseer_token'],
-                reason=error.get('Error', 'reason not specified'),
-                details=error.get('Cause', 'no details specified')
-            )
+            _mark_activity_failed(client, event, error)
             return
         client.respond_activity_task_completed(
             taskToken=event['overseer_token']
         )
+
+
+def _mark_activity_failed(client, event, error):
+    client.respond_activity_task_failed(
+        taskToken=event['overseer_token'],
+        reason=error.get('Error', 'reason not specified'),
+        details=error.get('Cause', 'no details specified')
+    )
